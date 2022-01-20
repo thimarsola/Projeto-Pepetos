8b75f7aec33d617ec9a2db74e9a3552f8890a7a5
thimarsola <tmarsola94@gmail.com>
2022-01-20 20:02:20 -0300

    conclusion of hero

---
+++ template-parts/home/home-hero.php
@@ -0,0 +1,20 @@
+<section class="hero">
+	<div class="container">
+        <!-- content -->
+         <div class="hero__content">
+
+             <!-- header -->
+              <header class="hero__content__header">
+                  <h2 class="f-7">O <span class="clr-primary-500 t-uppercase">melhor</span> lugar para o seu <span class="clr-primary-500 t-uppercase">pepeto</span>!</h2>
+                  <span class="line"></span>
+              </header>
+             <!-- end of header -->
+
+             <p class="mt-3 mb-4">Diversão, segurança, comodidade e muito amor em um só lugar.<br>Conheça tudo o que podemos oferecer para você e para seu Pepeto.</p>
+
+             <a href="#quem-somos" class="btn btn-primary-500">Saiba mais</a>
+
+         </div>
+        <!-- end of content -->
+    </div>
+</section>
